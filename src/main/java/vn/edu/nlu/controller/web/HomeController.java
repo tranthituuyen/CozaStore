028d55b3b57ca325ab201b98b498404524df1dfa
tranthituuyen <tranthituuyen@users.noreply.github.com>
2020-12-27 10:31:03 +0700

    .

--- src/main/java/vn/edu/nlu/controller/web/HomeController.java
+++ src/main/java/vn/edu/nlu/controller/web/HomeController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/trang-chu")
+@WebServlet(urlPatterns = "/trang-chu", name = "trang chu")
 public class HomeController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
