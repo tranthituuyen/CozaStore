d985b9168a988ee2753e4fc29c522747cac704cc
tranthituuyen <tranthituuyen@users.noreply.github.com>
2020-12-27 14:27:11 +0700

    add admin controller

---
+++ src/main/java/vn/edu/nlu/controller/admin/CategoryController.java
@@ -0,0 +1,21 @@
+package vn.edu.nlu.controller.admin;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/admin-category-management"}, name = "admin/category")
+public class CategoryController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/admin/category-management.jsp");
+        requestDispatcher.forward(request, response);
+    }
+}
---
+++ src/main/java/vn/edu/nlu/controller/admin/DealController.java
@@ -0,0 +1,21 @@
+package vn.edu.nlu.controller.admin;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/admin-deal-management"}, name = "admin/deal")
+public class DealController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/views/admin/deal-management.jsp");
+        requestDispatcher.forward(request, response);
+    }
+}
