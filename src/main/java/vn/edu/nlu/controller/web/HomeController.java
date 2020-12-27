c8056c10c12be6686be29069f8cce0f110133316
tranthituuyen <tranthituuyen@users.noreply.github.com>
2020-12-27 10:07:23 +0700

    update

--- src/main/java/vn/edu/nlu/controller/web/HomeController.java
+++ src/main/java/vn/edu/nlu/controller/web/HomeController.java
@@ -7,13 +7,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = "/${Entity_Name}")
+@WebServlet(urlPatterns = "/trang-chu")
 public class HomeController extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String context = request.getContextPath();
+        response.sendRedirect(context + "/views/web/home.jsp");
     }
 }
---
+++ src/main/java/vn/edu/nlu/controller/web/LoginController.java
@@ -0,0 +1,20 @@
+package vn.edu.nlu.controller.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/dang-nhap")
+public class LoginController extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String context = request.getContextPath();
+        response.sendRedirect(context + "/views/web/login.jsp");
+    }
+}
