f528d8a04d871feb5a6a867d642cebebab4d67cc
tranthituuyen <tranthituuyen@users.noreply.github.com>
2020-12-31 20:46:54 +0700

    add sql

--- database/create_table.sql
+++ database/create_table.sql
@@ -25,3 +25,45 @@ CREATE TABLE user (
 
 ALTER TABLE user ADD CONSTRAINT fk_user_role FOREIGN KEY (roleid) REFERENCES role(id);
 
+# chitietsanpham
+CREATE TABLE `chitietsanpham`  (
+  `machitietsanpham` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `masanpham` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `size` varchar(8) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `mau` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `soluong` int NOT NULL,
+  `ngaynhap` datetime NOT NULL,
+  PRIMARY KEY (`machitietsanpham`) USING BTREE,
+  INDEX `fk_msp`(`masanpham`) USING BTREE,
+  CONSTRAINT `fk_msp` FOREIGN KEY (`masanpham`) REFERENCES `sanpham` (`masanpham`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+# danhmuc
+CREATE TABLE `danhmuc`  (
+  `madanhmuc` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `tendanhmuc` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `hinhanh` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`madanhmuc`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+#
+CREATE TABLE `hinhanhsanpham`  (
+  `stt` int NOT NULL AUTO_INCREMENT,
+  `masanpham` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `hinhanhsanpham` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`stt`, `masanpham`) USING BTREE,
+  INDEX `fk_masp`(`masanpham`) USING BTREE,
+  CONSTRAINT `fk_masp` FOREIGN KEY (`masanpham`) REFERENCES `sanpham` (`masanpham`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB AUTO_INCREMENT = 78 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+#
+CREATE TABLE `sanpham`  (
+  `masanpham` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `madanhmuc` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `tensanpham` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `gia` int NOT NULL,
+  `mota` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  PRIMARY KEY (`masanpham`) USING BTREE,
+  INDEX `fk_mdm`(`madanhmuc`) USING BTREE,
+  CONSTRAINT `fk_mdm` FOREIGN KEY (`madanhmuc`) REFERENCES `danhmuc` (`madanhmuc`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
