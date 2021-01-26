f870bf5b8ef46b0b49d711f09caf618a2290cc48
heocoibebong13 <16130459@st.hcmuaf.edu.vn>
2021-01-27 05:25:40 +0700

    giỏ hàng

---
+++ src/main/java/vn/edu/nlu/controller/web/Cart.java
@@ -0,0 +1,76 @@
+package vn.edu.nlu.controller.web;
+
+import vn.edu.nlu.dao.impl.ProductDAO;
+import vn.edu.nlu.model.Item;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.*;
+
+@WebServlet(urlPatterns = {"/cart"})
+public class Cart extends HttpServlet {
+
+    public Cart() {
+        super();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if(action==null){
+            doGet_DisplayCart(request,response);
+        }else {
+            if(action.equalsIgnoreCase("buy")){
+                doGet_Buy(request,response);
+            }else if(action.equalsIgnoreCase("remove")){
+                doGet_Remove(request,response);
+            }
+        }
+    }
+    protected void doGet_DisplayCart(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            request.getRequestDispatcher("/views/web/shopping-cart.jsp").forward(request,response);
+    }
+    protected void doGet_Remove(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        List<Item> cart = (List<Item>) session.getAttribute("cart");
+        int index = isExisting(request.getParameter("id"),cart);
+        cart.remove(index);
+        session.setAttribute("cart",cart);
+        response.sendRedirect("cart");
+    }
+    protected void doGet_Buy(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ProductDAO productDAO = new ProductDAO();
+        HttpSession session = request.getSession();
+        if(session.getAttribute("cart")==null){
+            List<Item> cart = new ArrayList<Item>();
+            cart.add(new Item(productDAO.findOne(request.getParameter("id")),1));
+            session.setAttribute("cart",cart);
+        }else {
+            List<Item> cart = (List<Item>) session.getAttribute("cart");
+            int index = isExisting(request.getParameter("id"),cart);
+            if(index==-1){
+                cart.add(new Item(productDAO.findOne(request.getParameter("id")),1));
+            }else {
+                int quantity = cart.get(index).getQuantity()+1;
+                cart.get(index).setQuantity(quantity);
+            }
+            session.setAttribute("cart",cart);
+        }
+        response.sendRedirect("cart");
+    }
+    private int isExisting(String id, List<Item> cart){
+        for(int i=0;i<cart.size();i++){
+            if(cart.get(i).getProduct().getCode().equalsIgnoreCase(id)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
